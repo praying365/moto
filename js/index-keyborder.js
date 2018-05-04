e2c25642a49bb887281267c08cc623ce1e5346e8
Ning <praying365@gmail.com>
2018-05-04 14:41:13 +0800

    Add files via upload

---
+++ js/index-keyborder.js
@@ -0,0 +1,34 @@
+$(function(){
+//	$(".nav-top").delay(3000).animate({
+//		"top":"0px",
+//		"delay":"3s",
+//		"opacity":"1"
+//		},3000)
+	
+		$(".nav-top h2").animate({
+		"top":"-60px",
+		"opacity":"1",
+		},1500)
+//		
+		$(".nav-top h3").delay(400).animate({
+		"top":"20px",
+		"opacity":"1",
+		},1000)
+		
+		$(".nav-top p").delay(600).animate({
+		"top":"100px",
+		"opacity":"1",
+		},1000)
+		
+		$(".nav-top .btn01").delay(700).animate({
+		"top":"240px",
+		"opacity":"1",
+		},1000)
+		
+		
+		$(".nav-down").delay(400).animate({
+		"top":"420px",
+		"opacity":"1"
+		},2000)
+		
+})
---
+++ js/index-keyborder1.js
@@ -0,0 +1,44 @@
+$(function(){
+//	$(".nav-top").delay(3000).animate({
+//		"top":"0px",
+//		"delay":"3s",
+//		"opacity":"1"
+//		},3000)
+	
+		$(".nav-top h2").animate({
+		"top":"140px",
+		"opacity":"1",
+		},1500)
+//		
+		$(".nav-top h3").delay(400).animate({
+		"top":"220px",
+		"opacity":"1",
+		},1000)
+		
+		$(".nav-top p").delay(600).animate({
+		"top":"280px",
+		"opacity":"1",
+		},1000)
+		
+		$(".nav-top .btn01").delay(700).animate({
+		"top":"420px",
+		"opacity":"1",
+		},1000)
+		
+		
+
+		
+//		$(".product-left").animate({
+//			"top":"200px",
+//			"opacity":"1"
+//		},1500)
+		
+//		$(" .product-left").delay(400).animate({
+//			"left":"100px"
+//		},1500)
+		
+		$(" .product-right").delay(2500).animate({
+			"top":"35%",
+			"opacity":"1"
+		},1500)		
+})
